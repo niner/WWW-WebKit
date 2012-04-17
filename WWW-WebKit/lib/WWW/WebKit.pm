5c4ef00af5bda5cd6c462f41f26667b9aa4afc35
Stefan Seifert <nine@detonation.org>
2012-04-17 09:30:01 +0200

    Version 0.02
    Added README and Changes files

--- WWW-WebKit/lib/WWW/WebKit.pm
+++ WWW-WebKit/lib/WWW/WebKit.pm
@@ -38,7 +38,7 @@ use X11::Xlib;
 use Carp qw(carp croak);
 use XSLoader;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use constant DOM_TYPE_ELEMENT => 1;
 use constant ORDERED_NODE_SNAPSHOT_TYPE => 7;
