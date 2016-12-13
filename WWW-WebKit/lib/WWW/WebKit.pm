2199ae2d38984316970de795a5514f85eb10b114
Stefan Seifert <nine@detonation.org>
2016-12-13 16:45:32 +0100

    Version 0.10

--- WWW-WebKit/lib/WWW/WebKit.pm
+++ WWW-WebKit/lib/WWW/WebKit.pm
@@ -40,7 +40,7 @@ use XSLoader;
 use English '-no_match_vars';
 use POSIX qw<F_SETFD F_GETFD FD_CLOEXEC>;
 
-our $VERSION = '0.09';
+our $VERSION = '0.10';
 
 use constant DOM_TYPE_ELEMENT => 1;
 use constant ORDERED_NODE_SNAPSHOT_TYPE => 7;
